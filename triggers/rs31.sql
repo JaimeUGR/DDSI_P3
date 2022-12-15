4b130ea1ce694b1c4960e54fff379d5e9558066a
Pelochus <anghflores@gmail.com>
2022-12-15 16:23:57 +0100

    Falta corregir y comprobar cosas en el trigger

--- triggers/rs31.sql
+++ triggers/rs31.sql
@@ -6,6 +6,7 @@
 
 -- Este trigger permite comprobar si un arbitro ya ha aceptado una oferta o no
 -- En caso que si haya aceptado se permite realizar oferta, en otro caso, no
+-- Para realizar esto, se cuenta cuantas ofertas aceptadas tiene el arbitro, si supera 0, ha aceptado
 
 CREATE OR REPLACE TRIGGER YA_ACEPTO_OFERTA
 	BEFORE
@@ -13,9 +14,16 @@ CREATE OR REPLACE TRIGGER YA_ACEPTO_OFERTA
 	FOR EACH ROW
 DECLARE
 	dni CHAR(9);
+	aceptadas INT;
 BEGIN
-	SELECT DNIArb, EstadoOferta INTO dni FROM OFERTAS_RECIBE_HECHA_UWU WHERE DNIArb = DNIArb2 -- Revisar esto, ¿como se llama la variable de entrada?
-	IF (EstadoOferta = "ACEPTADA")
+	-- TODO Importante, revisar como se llama la variable de entrada del dni del arbitro
+	-- TODO Hace falta incluir el CodEd actual? Tiene sentido, pero de donde lo saco
+	SELECT count(*) INTO aceptadas FROM OFERTAS_RECIBE_HECHA_UWU
+	WHERE DNIArb = dni AND EstadoOferta = "ACEPTADA";
+	IF (aceptadas > 0) THEN
+		raise_application_error(-20520, "Error al realizar oferta, el arbitro con DNI " || dni || " ya ha aceptado una")
+	END IF;
+	-- NOTE Nota para el futuro, sumar strings parece ser con || en PLSQL
 END;
 
 
