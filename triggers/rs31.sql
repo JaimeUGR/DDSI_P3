41ec0fbecb0db621c8433ce51353307370569e7c
Pelochus <anghflores@gmail.com>
2022-12-15 16:39:03 +0100

    Finalizado, falta comprobar si hace falta el Codigo de Edicion

--- triggers/rs31.sql
+++ triggers/rs31.sql
@@ -11,15 +11,13 @@ CREATE OR REPLACE TRIGGER YA_ACEPTO_OFERTA
 	BEFORE
 	INSERT ON OFERTAS_RECIBE_HECHA_UWU
 DECLARE
-	dni CHAR(9); -- Sospecho que hay que borrar esta variable y cambiarlo todo por new:DNIArb
 	aceptadas INT;
 BEGIN
-	-- TODO Importante, revisar como se llama la variable de entrada del dni del arbitro
 	-- TODO Hace falta incluir el CodEd actual? Tiene sentido, pero de donde lo saco
 	SELECT count(*) INTO aceptadas FROM OFERTAS_RECIBE_HECHA_UWU
-	WHERE DNIArb = dni AND EstadoOferta = "ACEPTADA";
+	WHERE DNIArb = :new.DNIArb AND EstadoOferta = "ACEPTADA";
 	IF (aceptadas > 0) THEN
-		raise_application_error(-20520, "Error al realizar oferta, el arbitro con DNI " || dni || " ya ha aceptado una")
+		raise_application_error(-20520, "Error al realizar oferta, el arbitro con DNI " || :new.DNIArb || " ya ha aceptado una")
 	END IF;
 	-- NOTE Nota para el futuro, sumar strings parece ser con || en PLSQL
 END;
